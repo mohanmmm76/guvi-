52b3971b6985be93ccf499320e082ad66938ca5b
mohanmmm765 <53172633+mohanmmm765@users.noreply.github.com>
2019-07-22 14:58:20 +0530

    Create vowelornot.py

---
+++ vowelornot.py
@@ -0,0 +1,7 @@
+m=input()
+if (m=="a" or m=="A" or m=="e" or m=="E" or m=="i" or m=="I" or m=="o" or m=="O" or m=="u" or m=="U"):
+    print("Vowel")
+elif (m>="a"and m<="z")or(m>="A"and m<="Z"):
+    print("Consonant")
+else:
+    print("Invalid")
