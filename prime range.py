b3a9286527e925667df45e1689deb83bbe2adace
mohanmmm765 <53172633+mohanmmm765@users.noreply.github.com>
2019-07-24 15:13:11 +0530

    Create prime range.py

---
+++ prime range.py
@@ -0,0 +1,8 @@
+m,u=map(int,input().split())  
+for n in range(m+1,u):  
+   if n>1:  
+       for i in range(2,n):  
+           if (n%i) == 0:  
+               break  
+       else:  
+          print (n,"",end="")
