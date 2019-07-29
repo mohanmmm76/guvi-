bc63d7a78a641a1b7bbb46891cd6246b21423e8f
mohanmmm765 <53172633+mohanmmm765@users.noreply.github.com>
2019-07-29 15:38:13 +0530

    Create commonstr.py

---
+++ commonstr.py
@@ -0,0 +1,12 @@
+m = int(input())
+s=[]
+for i in range(0,m):
+ lan=input()
+ s.append(lan)
+li=[]
+for i in zip(*s):
+ if(i.count(i[0])==len(i)):
+  li.append(i[0])
+ else:
+  break
+print(''.join(li))
